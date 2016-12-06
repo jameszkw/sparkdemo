2fefe3f51c723671298552197c93edc8f2d601a3
zhangkewu <zhangkewu@paypalm.cn>
2016-12-06 19:39:48 +0800

    sec

--- src/main/java/com/zkw/sparkdemo/sparkdemo/TranslationMethodRDD.java
+++ src/main/java/com/zkw/sparkdemo/sparkdemo/TranslationMethodRDD.java
@@ -1,10 +1,15 @@
 package com.zkw.sparkdemo.sparkdemo;
 
+import org.apache.commons.lang.StringUtils;
 import org.apache.spark.api.java.JavaRDD;
 import org.apache.spark.api.java.JavaSparkContext;
+import org.apache.spark.api.java.function.FlatMapFunction;
 import org.apache.spark.api.java.function.Function;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Iterator;
+import java.util.List;
 
 /**
  * RDD 转化操作方法demo
@@ -12,8 +17,51 @@ import java.util.Arrays;
 public class TranslationMethodRDD {
     private static JavaRDD<String> createData(){
         JavaSparkContext jsc = new JavaSparkContext("local", "createFromParallelize");
-        JavaRDD<String> lines = jsc.parallelize(Arrays.asList("hello","kitty","jay","james","jay1"));
+        JavaRDD<String> lines = jsc.parallelize(Arrays.asList("hello nina","kitty","jay zhou","james","jay1"));
         return lines;
+    }
+
+    /**
+     * map()方法的使用
+     */
+    private static void mapDemo(){
+        final JavaRDD<String> lines = createData();
+        JavaRDD<List> errorRDD = lines.map(new Function<String, List>() {
+            public List call(String s) throws Exception {
+                System.out.println("-----------------------"+s);
+                List list = new ArrayList();
+                list.add(s);
+                return list;
+            }
+        });
+        /*for (String str:errorRDD.collect()){
+            System.out.println(str);
+        }*/
+
+//        System.out.println(StringUtils.join(errorRDD.collect(),"_"));
+//        System.out.println(errorRDD.take(3));
+        System.out.println(errorRDD.collect());
+//        System.out.println(errorRDD.count());
+
+    }
+
+    /**
+     * flatMap()方法的使用
+     */
+    private static void flatMapDemo(){
+        JavaRDD<String> lines = createData();
+        JavaRDD<String> errorRDD = lines.flatMap(new FlatMapFunction<String, String>() {
+            public Iterator<String> call(String s) throws Exception {
+                System.out.println("-----------------------"+s);
+                List list = new ArrayList();
+                list.add(s);
+
+                return list.iterator();
+            }
+        });
+
+        System.out.println(errorRDD.collect());
+
     }
 
     /**
@@ -34,6 +82,7 @@ public class TranslationMethodRDD {
 
     /**
      * 方法union()的使用----组合两个RDD
+     * 方法take()
      */
     private static void unionDemo(){
         JavaRDD<String> lines = createData();
@@ -50,11 +99,17 @@ public class TranslationMethodRDD {
 
         JavaRDD<String> unionRDD = james.union(jay);
         for (String str:unionRDD.collect()){
-            System.out.println(str);
+            System.out.println("collect:"+str);
+        }
+
+        for (String str:unionRDD.take(2)){
+            System.out.println("take:"+str);
         }
     }
 
     public static void main(String[] args) {
-        unionDemo();
+//        unionDemo();
+//        mapDemo();
+        flatMapDemo();
     }
 }
